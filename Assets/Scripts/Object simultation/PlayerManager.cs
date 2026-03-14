9bb9c3f21ccc52213b66a753518060d08c7ed9ba
Toby Berriman <tb311524@falmouth.ac.uk>
2026-03-14 22:44:14 +0000

    Started making caltrop system... then mucked around with the main menu instead

--- Assets/Scripts/Object simultation/PlayerManager.cs
+++ Assets/Scripts/Object simultation/PlayerManager.cs
@@ -9,6 +9,9 @@ public class PlayerManager : MonoBehaviour
     public GameObject TileSelect;
     public GameObject TileSelectMask;
 
+    public int SelectedCaltrop;
+    public CaltropType[] CaltropCycle;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
