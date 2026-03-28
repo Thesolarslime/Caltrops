91633d1a789d7dcfa2b73c6823d55965eb52bab0
FoxySeven <foxysevenseven@gmail.com>
2026-03-28 18:12:07 +0000

    intro cutscene finished

--- Assets/Scripts/Small ones/CutsceneManager.cs
+++ Assets/Scripts/Small ones/CutsceneManager.cs
@@ -9,11 +9,22 @@ public class CutsceneManager : MonoBehaviour
     public SpriteRenderer image1;
     public SpriteRenderer image2;
     public SpriteRenderer image3;
+    public SpriteRenderer image4;
+    public SpriteRenderer image5;
+    public SpriteRenderer image6;
+    public SpriteRenderer image7;
+    public SpriteRenderer image8;
     public TMP_Text text1;
     public TMP_Text text2;
     public TMP_Text text3;
     public TMP_Text text4;
     public TMP_Text text5;
+    public TMP_Text text6;
+    public TMP_Text text7;
+    public TMP_Text text8;
+    public TMP_Text text9;
+    public TMP_Text text10;
+    public TMP_Text text11;
 
     void Start()
     {
@@ -104,6 +115,172 @@ public class CutsceneManager : MonoBehaviour
         {
             TextFadeIn(text4);
             if (counter == 1) { cutsceneStep += 1; counter = 0; }
+        }
+        //WAIT
+        if (cutsceneStep == 10)
+        {
+            counter += 0.2f * Time.deltaTime;
+            if (counter > 1) { cutsceneStep += 1; counter = 1; }
+        }
+        //TEXT 4 HIDE
+        if (cutsceneStep == 11)
+        {
+            FadeOut(text4);
+            if (counter == 0) { cutsceneStep += 1;}
+        }
+        //TEXT 5 SHOW
+        if (cutsceneStep == 12)
+        {
+            TextFadeIn(text5);
+            if (counter == 1) { cutsceneStep += 1; counter = 0; }
+        }
+        //IMAGE 4 SHOW
+        if (cutsceneStep == 13)
+        {
+            ImageFadeIn(image4);
+            if (counter == 1) { cutsceneStep += 1;}
+        }
+        //TEXT 5 HIDE
+        if (cutsceneStep == 14)
+        {
+            FadeOut(text5);
+            if (counter == 0) { cutsceneStep += 1;}
+        }
+        //TEXT 6 SHOW
+        if (cutsceneStep == 15)
+        {
+            TextFadeIn(text6);
+            if (counter == 1) { cutsceneStep += 1; counter = 0; }
+        }
+        //IMAGE 5 SHOW
+        if (cutsceneStep == 16)
+        {
+            ImageFadeIn(image5);
+            if (counter == 1) { cutsceneStep += 1;}
+        }
+        //WAIT
+        if (cutsceneStep == 17)
+        {
+            counter += 0.5f * Time.deltaTime;
+            if (counter > 1) { cutsceneStep += 1; counter = 1; }
+        }
+        //TEXT 6 HIDE
+        if (cutsceneStep == 18)
+        {
+            FadeOut(text6);
+            if (counter == 0) { cutsceneStep += 1; }
+        }
+        //TEXT 7 SHOW
+        if (cutsceneStep == 19)
+        {
+            TextFadeIn(text7);
+            if (counter == 1) { cutsceneStep += 1; counter = 0; }
+        }
+        //IMAGE 6 SHOW
+        if (cutsceneStep == 20)
+        {
+            ImageFadeIn(image6);
+            if (counter == 1) { cutsceneStep += 1; }
+        }
+        //WAIT
+        if (cutsceneStep == 21)
+        {
+            counter += 0.5f * Time.deltaTime;
+            if (counter > 1) { cutsceneStep += 1; counter = 1; }
+        }
+        //TEXT 7 HIDE
+        if (cutsceneStep == 22)
+        {
+            FadeOut(text7);
+            if (counter == 0) { cutsceneStep += 1; }
+        }
+        //TEXT 8 SHOW
+        if (cutsceneStep == 23)
+        {
+            TextFadeIn(text8);
+            if (counter == 1) { cutsceneStep += 1; counter = 0; }
+        }
+        //WAIT
+        if (cutsceneStep == 24)
+        {
+            counter += 0.4f * Time.deltaTime;
+            if (counter > 1) { cutsceneStep += 1; counter = 1; }
+        }
+        //TEXT 8 HIDE
+        if (cutsceneStep == 25)
+        {
+            FadeOut(text8);
+            if (counter == 0) { cutsceneStep += 1; }
+        }
+        //TEXT 9 SHOW
+        if (cutsceneStep == 26)
+        {
+            TextFadeIn(text9);
+            if (counter == 1) { cutsceneStep += 1; counter = 0; }
+        }
+        //IMAGE 7 SHOW
+        if (cutsceneStep == 27)
+        {
+            ImageFadeIn(image7);
+            if (counter == 1) { cutsceneStep += 1; }
+        }
+        //WAIT
+        if (cutsceneStep == 28)
+        {
+            counter += 0.5f * Time.deltaTime;
+            if (counter > 1) { cutsceneStep += 1; counter = 1; }
+        }
+        //TEXT 9 HIDE
+        if (cutsceneStep == 29)
+        {
+            FadeOut(text9);
+            if (counter == 0) { cutsceneStep += 1; }
+        }
+        //TEXT 10 SHOW
+        if (cutsceneStep == 30)
+        {
+            TextFadeIn(text10);
+            if (counter == 1) { cutsceneStep += 1; counter = 0; }
+        }
+        //WAIT
+        if (cutsceneStep == 31)
+        {
+            counter += 0.2f * Time.deltaTime;
+            if (counter > 1) { cutsceneStep += 1; counter = 1; }
+        }
+        //TEXT 10 HIDE
+        if (cutsceneStep == 32)
+        {
+            FadeOut(text10);
+            if (counter == 0) { cutsceneStep += 1; }
+        }
+        //TEXT 11 SHOW
+        if (cutsceneStep == 33)
+        {
+            TextFadeIn(text11);
+            if (counter == 1) { cutsceneStep += 1;}
+        }
+        if (cutsceneStep == 34)
+        {
+            image1.color = new Color(1.0f, 1.0f, 1.0f, 0);
+            image2.color = new Color(1.0f, 1.0f, 1.0f, 0);
+            image3.color = new Color(1.0f, 1.0f, 1.0f, 0);
+            image4.color = new Color(1.0f, 1.0f, 1.0f, 0);
+            image5.color = new Color(1.0f, 1.0f, 1.0f, 0);
+            image6.color = new Color(1.0f, 1.0f, 1.0f, 0);
+            cutsceneStep += 1;
+        }
+        if (cutsceneStep == 35)
+        {
+            counter += -0.2f * Time.deltaTime;
+            if (counter < 0) { counter = 0; }
+            image7.color = new Color(1.0f, 1.0f, 1.0f, counter);
+            text11.color = new Color(1.0f, 1.0f, 1.0f, counter);
+            if (counter == 0) { cutsceneStep += 1; counter = 1; }
+        }
+        if (cutsceneStep == 36)
+        {
+
         }
     }
 }
