a2858dfc5d52a832895315577c07fc955c454dea
Toby Berriman <tb311524@falmouth.ac.uk>
2026-03-29 13:34:47 +0100

    LevelupBonuses upscaled and levelup sequence worked on

--- Assets/Scripts/LevelUpChoiceBox.cs
+++ Assets/Scripts/LevelUpChoiceBox.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class LevelUpChoiceBox : MonoBehaviour
 {
+    public LevelBonus Bonus;
+    public CaltropType Caltrop;
+
+    public Animator BoxAnimator;
+    public Animator SpriteAnimator;
+
+    public LevelUpManager LevelUpManager;
+    public int ID;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +20,18 @@ public class LevelUpChoiceBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (LevelUpManager.MenuActive)
+        {
+            if (LevelUpManager.SelectedBox == ID)
+            {
+                BoxAnimator.SetBool("Selected", true);
+                SpriteAnimator.SetBool("Selected", true);
+            }
+            else
+            {
+                BoxAnimator.SetBool("Selected", false);
+                SpriteAnimator.SetBool("Selected", false);
+            }
+        }
     }
 }
--- Assets/Scripts/LevelUpManager.cs
+++ Assets/Scripts/LevelUpManager.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelUpManager : MonoBehaviour
@@ -6,6 +9,11 @@ public class LevelUpManager : MonoBehaviour
     public Animator MenuAnimator;
 
     public LevelUpChoiceBox[] ChoiceBoxes;
+    public SpriteRenderer[] ChoiceSprites;
+    public int SelectedBox;
+
+    public int LevelUpPhase; // 0 = intro anim, 1 = bonus choice, 2 = caltrop choice, 3 = replace choice
+    private bool SelectionMade; // becomes true to signal that a choice has been made and it's time to move on with the levelup sequence.
 
     public CaltropType[] PossibleCaltrops;
     public LevelBonus[] PossibleBonuses;
@@ -32,14 +40,57 @@ public class LevelUpManager : MonoBehaviour
         }
     }
 
-    public void PickCaltropOptions()
+    public void PickOptions(int phase)
     {
+        if (phase == 1)
+        {
+            List<int> Bonuses = new List<int>();
+            Bonuses.Add(0); Bonuses.Add(1); Bonuses.Add(2); Bonuses.Add(3); Bonuses.Add(4); Bonuses.Add(5);
 
+            // a really unoptimised but simple way of getting 3 random bonuses
+            int ChosenBonus = Random.Range(0, Bonuses.Count);
+            ChoiceBoxes[0].Bonus = PossibleBonuses[ChosenBonus];
+            Bonuses.RemoveAt(ChosenBonus);
+            ChosenBonus = Random.Range(0, Bonuses.Count);
+            ChoiceBoxes[1].Bonus = PossibleBonuses[ChosenBonus];
+            Bonuses.RemoveAt(ChosenBonus);
+            ChosenBonus = Random.Range(0, Bonuses.Count);
+            ChoiceBoxes[2].Bonus = PossibleBonuses[ChosenBonus];
+            Bonuses.RemoveAt(ChosenBonus);
+        }
+        
     }
 
-    public void LevelUpSequence()
+    public IEnumerator LevelUpSequence()
     {
+        LevelUpPhase = 0;
+        MenuActive = true;
         MenuAnimator.SetBool("MenuUp", true);
         this.gameObject.SetActive(true);
+        SelectedBox = 3;
+        yield return new WaitForSeconds(1);
+        PickOptions(1);
+        SelectedBox = 1;
+        LevelUpPhase = 1;
+        Selection();
+    }
+
+    public void Selection()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (SelectedBox == 0) { SelectedBox = 2; }
+            else { SelectedBox--; }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (SelectedBox == 2) { SelectedBox = 0; }
+            else { SelectedBox++; }
+        }
+
+        if (MenuActive)
+        {
+            Selection();
+        }
     }
 }
